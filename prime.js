e8928a3efed9560f4c80390918c17d95b16bbc65
Divyansh <divyanshgoel777@gmail.com>
2022-11-03 22:42:21 +0530

    Code is resolved

--- prime.js
+++ prime.js
@@ -1,12 +1,12 @@
+
 let count=0;
-
-for(i=0;i<=23;i++){
+let num=23;
+for(let i=0;i<=23;i++){
 if(num%i==0){
-count
+count++;
 }
 }
-
-if(count=2){
+if(count==2){
 console.log(num."is prime");
 }else{
 console.log(num,"is not prime");
